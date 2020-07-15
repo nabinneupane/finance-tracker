d2ad4058f7e55f465ab10052322856655eb08ff5
Nabin Neupane <coolnabinn@nabins-mbp.attlocal.net>
2020-07-15 00:15:25 -0400

    Updates

---
+++ precache-manifest.65288d1f5aff46e36a3deafab20ac1d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0d4677bf9d63935777193b9af344d6",
+    "url": "/finance-tracker/index.html"
+  },
+  {
+    "revision": "2b759269e5a46263d1a1",
+    "url": "/finance-tracker/static/js/2.fe41490d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/finance-tracker/static/js/2.fe41490d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f475f487198f829776",
+    "url": "/finance-tracker/static/js/main.d9d394f0.chunk.js"
+  },
+  {
+    "revision": "7d4c68a3662a954d3980",
+    "url": "/finance-tracker/static/js/runtime-main.4d8e2ee1.js"
+  }
+]);
